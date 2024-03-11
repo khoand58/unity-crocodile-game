60e70b5f02480864d43c626aef07b0af683294ca
quanghuy6698 <quanghuy6698@gmail.com>
2024-03-11 21:51:54 +0700

    first commit

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject popupGameover;
+
+    [SerializeField]
+    private GameObject[] toothArr;
+
+    private int decayToothIndex;
+
+    void Start(){
+        decayToothIndex = Random.Range(0, 10);
+        popupGameover.SetActive(false);
+    }
+
+    public void onClickTooth(int toothIndex){
+        if(toothIndex == decayToothIndex){
+            popupGameover.SetActive(true);
+        } else {
+            toothArr[toothIndex].SetActive(false);
+        }
+    }
+
+    public void onPlayAgain(){
+        for(int i=0; i<10; i++){
+            toothArr[i].SetActive(true);
+        }
+        decayToothIndex = Random.Range(0, 10);
+        popupGameover.SetActive(false);
+    }
+}
